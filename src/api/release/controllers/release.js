0513a03efc5905ec195c3a2b73adb202673f4719
itlackey <itlackey@outlook.com>
2022-02-25 23:48:46 -0600

    some clean up

--- src/api/release/controllers/release.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  release controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::release.release');
--- src/api/release/routes/release.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * release router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::release.release');

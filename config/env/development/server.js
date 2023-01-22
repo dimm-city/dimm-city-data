d31421da1950a1d3379bb4d25d1b3bf653d3f1b8
itlackey <itlackey@outlook.com>
2023-01-21 23:31:43 -0600

    added dev env config

---
+++ config/env/development/server.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+    port: env.int('PORT', 8080),
+    production: false,
+    proxy: {
+        enabled: false
+    },
+    cron: {
+        enabled: false
+    },
+    admin: {
+        autoOpen: false,
+        auth: {
+            secret: "${process.env.ADMIN_JWT_SECRET} || 9518cfb7-49bb-4e6d-a518-b8de90c015a0"
+        }
+    }
+});

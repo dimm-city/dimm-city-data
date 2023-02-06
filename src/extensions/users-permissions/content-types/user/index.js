e812637498b5e597ffc7a2cb6ba0e1ba9f618164
itlackey <itlackey@outlook.com>
2023-02-06 00:07:20 -0600

    updating user to use wallet svc

--- src/extensions/users-permissions/content-types/user/index.js
+++ src/extensions/users-permissions/content-types/user/index.js
@@ -1,51 +1,14 @@
 //https://forum.strapi.io/t/strapi-create-new-user-users-permissions-plugin-lifecycles/13386/5
 const schema = require("./schema.json");
-const ethers = require("ethers");
 
 module.exports = {
   schema,
   lifecycles: {
     async afterCreate(event) {
-      console.log("afterCreate********************");
       const { result } = event;
-      const wallet = ethers.Wallet.createRandom();
-
       try {
-        // const existingUsers = await strapi.entityService.findMany(
-        //   "api::users-permissions.user",
-        //   {
-        //     filters: {
-        //       username: model.username,
-        //     },
-        //   }
-        // );
-        //console.log("existing user", result);
-
-        console.log(
-          "created new user",
-          result,
-          wallet.mnemonic.phrase,
-          wallet.privateKey
-        );
-
-        const walletEntity = await strapi.entityService.create(
-          "plugin::chain-wallets.chain-wallet",
-          {
-            data: {
-              managed: true,
-              address: wallet.address,
-              seed: wallet.mnemonic.phrase,
-              key: wallet.privateKey,
-              encKey: wallet.privateKey,
-              user: result,
-              blockchain: {
-                id: 2,
-              },
-            },
-          }
-        );
-
-        //console.log("wallet created", walletEntity);
+        const walletSvc = strapi.service("plugin::chain-wallets.chain-wallet");
+        walletSvc.createUserWallet(1, result);
       } catch (error) {
         console.log(error);
       }

5dab44bea5ea80324485fe69c1a0a03e50990ee7
itlackey <itlackey@outlook.com>
2023-03-04 02:18:42 -0600

    WIP: cleaning up character

--- src/api/character/routes/character.js
+++ src/api/character/routes/character.js
@@ -1,9 +1,28 @@
-'use strict';
+"use strict";
 
 /**
  * character router.
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::character.character');
+module.exports = createCoreRouter("api::character.character", {
+    only: ['find', 'findOne', 'update', 'create'],
+    config:{
+        update:{
+            policies:["api::character.owns-token"]
+        }
+    }
+});
+// module.exports = {
+//     routes: [
+//       {
+//         method: "PUT",
+//         path: "/characters/:id",
+//         handler: "character.update",
+//         config: {
+//           policies: ["api::character.owns-token"],
+//         },
+//       },
+//     ]
+// };

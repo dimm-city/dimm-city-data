6f3a2cc6f8da01ac9db891e3c7113daa273ebcf8
itlackey <itlackey@outlook.com>
2023-03-09 08:40:16 -0600

    db config clean up

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -12,3 +12,15 @@ module.exports = ({ env }) => ({
     debug: true,
   },
 });
+
+// const path = require('path');
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'sqlite',
+//     connection: {
+//       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+//     },
+//     useNullAsDefault: true,
+//   },
+// });

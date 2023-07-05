c550f93cc7ee23eee51d38d382d53b5930322498
itlackey <itlackey@outlook.com>
2023-07-04 19:52:00 -0500

    enabling az upload

--- config/plugins.js
+++ config/plugins.js
@@ -19,10 +19,14 @@ module.exports = ({ env }) => ({
   },
   upload: {
     config: {
+      provider: "strapi-provider-upload-azure-storage",
       providerOptions: {
-        localServer: {
-          maxage: 300000
-        },
+        account: env("STORAGE_ACCOUNT"),
+        accountKey: env("STORAGE_ACCOUNT_KEY"),
+        serviceBaseURL: env("STORAGE_URL"), // optional
+        containerName: env("STORAGE_CONTAINER_NAME"),
+        defaultPath: "media",
+        cdnBaseURL: env("STORAGE_CDN_URL"), // optional
       },
     },
   },

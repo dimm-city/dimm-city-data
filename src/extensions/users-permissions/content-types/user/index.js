14f351e7d72f1c76515865c23dda606d89109d8a
itlackey <itlackey@outlook.com>
2023-08-05 22:40:06 -0500

    fix user lifecycle, auto init profiles

--- src/extensions/users-permissions/content-types/user/index.js
+++ src/extensions/users-permissions/content-types/user/index.js
@@ -1,18 +1,19 @@
-// //https://forum.strapi.io/t/strapi-create-new-user-users-permissions-plugin-lifecycles/13386/5
-// const schema = require("./schema.json");
+//https://forum.strapi.io/t/strapi-create-new-user-users-permissions-plugin-lifecycles/13386/5
+const schema = require("./schema.json");
 
-// module.exports = {
-//   schema,
-//   lifecycles: {
-//     async afterCreate(event) {
-//       const { result } = event;
-//       try {
-//         await strapi.plugins["chain-wallets"].services[
-//           "chain-wallet"
-//         ].createManagedUserWallet(result, "mainnet");
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     },
-//   },
-// };
+module.exports = {
+  schema
+  //,
+  // lifecycles: {
+  //   async afterCreate(event) {
+  //     const { result } = event;
+  //     try {
+  //       await strapi.plugins["chain-wallets"].services[
+  //         "chain-wallet"
+  //       ].createManagedUserWallet(result, "mainnet");
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   },
+  // },
+};
--- src/plugins/dimm-city/server/bootstrap.js
+++ src/plugins/dimm-city/server/bootstrap.js
@@ -1,20 +1,14 @@
 "use strict";
 
-module.exports = ({ strapi }) => {
+module.exports = async ({ strapi }) => {
   // bootstrap phase
   strapi.db.lifecycles.subscribe({
     models: ["plugin::users-permissions.user"],
 
     async afterCreate(event) {
       const { result, params } = event;
-      console.log(
-        "################################### user created ##############################"
-      );
-      console.log(result);
 
       //Get chain-wallet service and create wallet for user
-      // const chainWalletService = strapi.plugin('chain-wallet').service('chain-wallet');
-      // chainWalletService.createWallet(result.id);
       await strapi.plugins["chain-wallets"].services[
         "chain-wallet"
       ].createManagedUserWallet(result, "mainnet");
@@ -29,4 +23,21 @@ module.exports = ({ strapi }) => {
       });
     },
   });
+
+  //load all users that do not have a profile
+  const users = await strapi.query("plugin::users-permissions.user").findMany({
+    where: {
+      profile: null,
+    },
+  });
+  for (let index = 0; index < users.length; index++) {
+    const element = users[index];
+    await strapi.plugins["dimm-city"].services.profile.create({
+      data: {
+        displayName: element.username,
+        email: element.email,
+        user: element.id,
+      },
+    });
+  }
 };

a80f69e75b5080ef3f12ef1339a94c8221f88dff
itlackey <itlackey@gmail.com>
2023-09-22 15:43:59 -0500

    mod startup to rebuild if needed

--- startup.sh
+++ startup.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+# Check if the node_modules folder contains any files or folders
+if [ -z "$(ls -A node_modules)" ]; then
+  echo "Missing node dependencies, running npm install"
+  npm install
+  echo "Running strapi build"
+  node node_modules/@strapi/strapi/bin/strapi.js build
+  echo "Strapi build complete, starting server"
+fi
 pm2 start server.js --no-daemon

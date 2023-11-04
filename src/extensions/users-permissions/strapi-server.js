1358abb1e1e8258bf4527b28ab67279ffb1a60fd
itlackey <itlackey@gmail.com>
2023-11-04 15:00:28 -0500

    create auth wrapper for now

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -4,5 +4,6 @@ module.exports = (plugin) => {
   plugin.contentTypes.user = user;
   plugin.bootstrap = require("./server/bootstrap-wrapper");
   plugin.services = require("./server/services");
+  plugin.controllers.auth = require("./server/controllers/auth");
   return plugin;
 };

2204395df29184e9bbb6499484c294fd0570ae5c
itlackey <itlackey@gmail.com>
2023-11-04 15:11:19 -0500

    aider: Added /home/founder3/code/dimm-city/dimm-city-data/src/extensions/users-permissions/server/controllers/profiles.js

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -4,6 +4,12 @@ module.exports = (plugin) => {
   plugin.contentTypes.user = user;
   plugin.bootstrap = require("./server/bootstrap-wrapper");
   plugin.services = require("./server/services");
-  plugin.controllers.auth = require("./server/controllers/auth");
+  plugin.controllers.profiles = require("./server/controllers/profiles");
+
+  plugin.routes["content-api"].routes.push({
+    method: "POST",
+    path: "/profiles/associate",
+    handler: "profiles.associateLogin",
+  });
   return plugin;
 };

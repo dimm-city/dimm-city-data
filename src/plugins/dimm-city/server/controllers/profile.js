86eecab2933a74abf6a87ce9a3f8dc567906a0b8
itlackey <itlackey@gmail.com>
2023-11-05 02:02:34 -0600

    updating response format for my characters

--- src/plugins/dimm-city/server/controllers/profile.js
+++ src/plugins/dimm-city/server/controllers/profile.js
@@ -10,23 +10,34 @@ module.exports = createCoreController(
   "plugin::dimm-city.profile",
   ({ strapi }) => ({
     async characters(ctx) {
-      const userId = ctx.state?.user?.id;
-      const results = await strapi
-        .service("plugin::dimm-city.character")
-        .find({
-          filters: {
-            token: {
-              wallet: {
-                profile: {
-                  users: { id: userId },
+      try {
+        const userId = ctx.state?.user?.id;
+
+        // sanitizeQuery to remove any query params that are invalid or the user does not have access to
+        // It is strongly recommended to use sanitizeQuery even if validateQuery is used
+        const sanitizedQueryParams = await this.sanitizeQuery(ctx);
+        const { results, pagination } = await strapi
+          .service("plugin::dimm-city.character")
+          .find({
+            filters: {
+              token: {
+                wallet: {
+                  profile: {
+                    users: { id: userId },
+                  },
                 },
               },
             },
-          },
-          populate: "*"
-        });
-      ctx.send(results);
-      //TODO: move this to character controller to get teh correst rest response
+            populate: "*",
+          });
+        const sanitizedResults = await this.sanitizeOutput(results, ctx);
+
+        return this.transformResponse(sanitizedResults, { pagination });
+
+        //TODO: move this to character controller to get teh correst rest response
+      } catch (error) {
+        ctx.send({ message: "Could not load characters", error: error });
+      }
     },
   })
 );
